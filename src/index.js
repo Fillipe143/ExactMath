19621ef7788abb0fe1803cc2a688beb6acea36b6
Fillipe143 <fillipefbatist@gmail.com>
2022-12-11 18:26:59 -0300

    added subtract function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ function sum(num1, num2) {
     return (num1 * multiple + num2 * multiple) / multiple;
 }
 
+/**
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns {number}
+ */
+function subtract(num1, num2) {
+    const decimalPlaces = getMaxDecimalPlaces(num1, num2);
+    const multiple = 10 ** decimalPlaces;
+
+    return (num1 * multiple - num2 * multiple) / multiple
+}
+
+
 module.exports = {
-    sum
+    sum,
+    subtract
 }
